673855bb5b9823fec34d3b09363e178ba0a558d4
enesarii <enesari@outlook.com.tr>
2024-11-20 23:56:15 +0300

    Update odev_1.cs

--- odev_1.cs
+++ odev_1.cs
@@ -2,14 +2,14 @@
 {
     private static void Main(string[] args)
     {
-        Console.Write("Bir sayı girin: ");
+        Console.Write("Bir sayı girin: "); //kullanıcıdan veri alma
         int a, b;
-        b = Convert.ToInt32(Console.ReadLine());
-        int toplam = 0;
-        for (a = 1; a <= b; a++)
+        b = Convert.ToInt32(Console.ReadLine()); //aldığın string veriyi int'e dönüştürme
+        int toplam = 0; // değişkeni sıfır olarak başlatmak için
+        for (a = 1; a <= b; a++) //1den b ye olan sayılarla döngü başlatmak için
         {
-            toplam += a;
+            toplam += a; //a değerini toplam'a eklemek için 
         }
-        Console.WriteLine(" Girdiğiniz sayıya kadar olan tüm sayıların toplamı : {1}", b, toplam);
+        Console.WriteLine(" Girdiğiniz sayıya kadar olan tüm sayıların toplamı : {1}", b, toplam); //sonucu ekrana yazdırmak için
     }
 }
