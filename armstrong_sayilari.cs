cdf5eaa02ebfd4fd6dab62a257b48b2def8ea1e3
enesarii <enesari@outlook.com.tr>
2024-11-21 00:05:47 +0300

    Update armstrong_sayilari.cs

--- armstrong_sayilari.cs
+++ armstrong_sayilari.cs
@@ -2,26 +2,24 @@
 {
     private static void Main(string[] args)
     {
-        Console.Write("Bir sayı girin: ");
+        Console.Write("Bir sayı girin: "); //kullanıcıdan veri istenir
         int sayi;
-        sayi = Convert.ToInt32(Console.ReadLine());
-        int basamakSayisi = sayi.ToString().Length;
-        int girilensayi = sayi;
-        int toplam = 0;
-        while (girilensayi > 0)
+        sayi = Convert.ToInt32(Console.ReadLine()); //istenen veri sayi değişkenine atanır
+        int basamakSayisi = sayi.ToString().Length; // Basamak sayısını bulmak için sayıyı string dönüştürüyoruz ve uzunluğunu alıyoruz
+        int girilensayi = sayi; // sayıyı basamağı sonradan ayarlamak için geçici bir değişkene atıyoruz
+        int toplam = 0; // toplamı sıfırlıyoruz
+        while (girilensayi > 0) //sayı sıfır olana kadar her basamağa işlem yapılır
         {
-            int basamak = girilensayi % 10;
-            toplam += (int)Math.Pow(basamak, basamakSayisi);
-            girilensayi /= 10;
+            int basamak = girilensayi % 10; //sayının son basamağını almak için
+            toplam += (int)Math.Pow(basamak, basamakSayisi);// basamağın basamak sayısı kadar üssünü alıyoruz ve toplamla topluyoruz
+            girilensayi /= 10;//sayının son basamağını çıkarıyoruz
         }
-        if (toplam == sayi)
+        if (toplam == sayi) //eğer toplam ilk sayıya eşitse bu sayı armstrong sayısıdır.
         {
             Console.WriteLine("Girdiğinz sayı Armstrong sayısıdır.");
         }
         else
-
             Console.WriteLine("Girdiğiniz sayı bir Armstrong sayısı değildir.");
     }
 }
-    }
-}
+    
