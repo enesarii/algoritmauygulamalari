7f13d9c8a9a1d0b49ba683164ae86460317e3da7
enesarii <enesari@outlook.com.tr>
2024-11-20 22:23:10 +0300

    uygulama 1
    221120241004

---
+++ uyg1_geoortalama.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Kullanıcıdan bilgi al
+        Console.WriteLine("Birinci sayıyı girin:");
+        double sayi1 = Convert.ToDouble(Console.ReadLine()); // küsürat olabilir o yüzden double kullanıyoruz
+        Console.WriteLine("İkinci sayıyı girin:");
+        double sayi2 = Convert.ToDouble(Console.ReadLine());
+        
+        double geometrikOrtalama = Math.Sqrt(sayi1 * sayi2); // Geometrik ortalamayı hesaplatıyoruz
+
+        Console.WriteLine("Geometrik Ortalama: " + geometrikOrtalama);// Sonucu ekrana yazdır
+    }
---
+++ uyg1_kullanicidanbilgial.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string ad, soyad, tel, no;
+
+        // Kullanıcıdan bilgileri almak için 
+        Console.Write("Adınızı girin: ");
+        ad = Console.ReadLine();
+
+        Console.Write("Soyadınızı girin: ");
+        soyad = Console.ReadLine();
+
+        Console.Write("Öğrenci numaranızı girin: ");
+        no = Console.ReadLine();
+
+        Console.Write("Cep telefonunuzu girin: ");
+        tel = Console.ReadLine();
+
+       
+        Console.WriteLine("Kullanıcı Bilgileriniz:");  // Bilgileri yazdırmak için 
+        Console.WriteLine("Ad: " + ad);
+        Console.WriteLine("Soyad: " + soyad);
+        Console.WriteLine("Öğrenci Numarası: " + no);
+        Console.WriteLine("Cep Telefonu: " + tel);
+    }
+}
