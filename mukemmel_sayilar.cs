d3079198558e3ff3c7e840821e05ddb6801b5872
enesarii <enesari@outlook.com.tr>
2024-11-20 22:42:37 +0300

    Add files via upload

---
+++ mukemmel_sayilar.cs
@@ -0,0 +1,26 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Bir sayı giriniz");
+        int sayi, i, bolentoplam;
+        bolentoplam = 0;
+        sayi = Convert.ToInt16(Console.ReadLine());
+        for (i = 1; i < sayi; i++)
+        {
+            if (sayi % i == 0)
+            {
+                bolentoplam += i;
+            }
+        }
+        if (bolentoplam == sayi)
+        {
+            Console.WriteLine("Bu sayı mükemmel bir sayıdır.");
+        }
+        else
+        {
+            Console.WriteLine("Bu sayı mükemmel sayı değildir.");
+        }
+        Console.ReadLine();
+    }
+}
