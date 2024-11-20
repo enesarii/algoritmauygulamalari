6412dfb5fad99b276f40cca98c605b54cb671b6b
enesarii <enesari@outlook.com.tr>
2024-11-20 22:42:51 +0300

    Add files via upload

---
+++ armstrong_sayilari.cs
@@ -0,0 +1,27 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Bir sayı girin: ");
+        int sayi;
+        sayi = Convert.ToInt32(Console.ReadLine());
+        int basamakSayisi = sayi.ToString().Length;
+        int girilensayi = sayi;
+        int toplam = 0;
+        while (girilensayi > 0)
+        {
+            int basamak = girilensayi % 10;
+            toplam += (int)Math.Pow(basamak, basamakSayisi);
+            girilensayi /= 10;
+        }
+        if (toplam == sayi)
+        {
+            Console.WriteLine("Girdiğinz sayı Armstrong sayısıdır.");
+        }
+        else
+
+            Console.WriteLine("Girdiğiniz sayı bir Armstrong sayısı değildir.");
+    }
+}
+    }
+}
