faf22b5542c5dc5bac40a0802d6f9e726156dd45
enesarii <enesari@outlook.com.tr>
2024-11-20 22:37:34 +0300

    Akış Diyagramı İlk Örnek
    25 Ekim

---
+++ akis_diyagrami_ilkornek.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        Console.Write("a değerini girin: ");
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("b değerini girin: ");
+        int b = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("c değerini girin: ");
+        int c = Convert.ToInt32(Console.ReadLine());
+
+
+        int toplam = a + b + c;
+
+
+        if (toplam < 180)
+        {
+            Console.WriteLine("Üçgen değil");
+        }
+        else if (toplam > 180)
+        {
+            Console.WriteLine("Üçgen değil");
+        }
+        else
+        {
+            Console.WriteLine("Üçgen");
+        }
+
+
+        Console.WriteLine("Program sonlandı.");
+    }
+}
