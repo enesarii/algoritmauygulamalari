38b308957de37d4b897d7f2134010fdd92e9bc1f
enesarii <enesari@outlook.com.tr>
2024-11-20 22:40:43 +0300

    Ödev 1
    19 Kas 2024 Sal 14:00

---
+++ odev_1.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Bir sayı girin: ");
+        int a, b;
+        b = Convert.ToInt32(Console.ReadLine());
+        int toplam = 0;
+        for (a = 1; a <= b; a++)
+        {
+            toplam += a;
+        }
+        Console.WriteLine(" Girdiğiniz sayıya kadar olan tüm sayıların toplamı : {1}", b, toplam);
+    }
+}
