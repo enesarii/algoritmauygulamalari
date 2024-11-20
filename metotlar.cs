6217bc4fa57ef506e25b7e52390650c0c6853eed
enesarii <enesari@outlook.com.tr>
2024-11-20 22:33:51 +0300

    metotlar ödevi
    15 ekim

---
+++ metotlar.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace enesarimetot
+{
+    internal class Program
+    {
+        static void Main(string [enesarimetot] args)
+        {
+            //öğrenci  bilgisi al 
+            Console.Write("ögrenci isim soyisim: ");
+            string adSoyad = Console.ReadLine();
+
+            //1. sınav not bilgisi al 
+            Console.Write("1. sınav notu: ");
+            int not1 = Convert.ToInt32(Console.ReadLine());
+            //2. sınav not bilgisi al 
+            Console.Write("2. sınav notu: ");
+            int not2 = Convert.ToInt32(Console.ReadLine());
+            //ortalamayı hesapla
+            int ortalama = (not1 + not2) / 2;
+
+            //öğrenci bilgisiyle birlikte ortalamayı göster
+            Console.WriteLine($"{adSoyad} için not ortalaması: {ortalama}");
+        }
+    }
+}
