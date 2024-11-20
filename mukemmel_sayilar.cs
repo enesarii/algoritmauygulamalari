635c49d3e39bd8dc6e5ba1a8019a9996cf13a20c
enesarii <enesari@outlook.com.tr>
2024-11-21 00:00:10 +0300

    Update mukemmel_sayilar.cs

--- mukemmel_sayilar.cs
+++ mukemmel_sayilar.cs
@@ -3,17 +3,17 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Bir sayı giriniz");
-        int sayi, i, bolentoplam;
-        bolentoplam = 0;
-        sayi = Convert.ToInt16(Console.ReadLine());
-        for (i = 1; i < sayi; i++)
+        int sayi, i, bolentoplam; 
+        bolentoplam = 0; //bölenlerin toplamını başlatmak için 
+        sayi = Convert.ToInt16(Console.ReadLine()); // Kullanıcıdan sayıyı alıyoruz
+        for (i = 1; i < sayi; i++)         // 1'den sayi - 1'e kadar olan sayılarla döngü yapıp bölenleri buluyoruz
         {
-            if (sayi % i == 0)
+            if (sayi % i == 0)// Eğer 'i' sayısının böleni ise
             {
-                bolentoplam += i;
+                bolentoplam += i; // toplama ekliyoruz
             }
         }
-        if (bolentoplam == sayi)
+        if (bolentoplam == sayi) // Eğer bölenlerin toplamı sayıya eşitse, sayı mükemmel bir sayıdır
         {
             Console.WriteLine("Bu sayı mükemmel bir sayıdır.");
         }
